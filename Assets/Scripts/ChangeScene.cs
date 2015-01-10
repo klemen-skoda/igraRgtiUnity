9f7cf8f3288f7f0d5f2e506882e011f32ecf0c5a
Mitja <mitja_rizvic@hotmail.com>
2015-01-10 21:47:45 +0100

    Dodan zacetni meni

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Update is called once per frame
+	public void changeScene (int scene) {
+		Application.LoadLevel(scene);
+	}
+
+	public void exitGame ()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetButtonDown( "Cancel" )) {
+			Application.LoadLevel(0);
+		}
+	}
+}
