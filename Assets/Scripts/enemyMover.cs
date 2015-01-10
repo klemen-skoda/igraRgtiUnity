a82c02542caf7714508b77ee1d6a40e5a0caba2e
klemen-skoda <klemen.skoda@gmail.com>
2015-01-10 18:49:05 +0100

    dodani nasprotniki ter sledenje igralcu

---
+++ Assets/Scripts/enemyMover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMover : MonoBehaviour
+{
+	Transform player;
+	//PlayerHealth playerHealth;
+	//EnemyHealth enemyHealth;
+	NavMeshAgent nav;
+	
+	
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		//playerHealth = player.GetComponent <PlayerHealth> ();
+		//enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <NavMeshAgent> ();
+	}
+	
+	
+	void Update ()
+	{
+		//if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+		//{
+		nav.SetDestination (player.position);
+		if (nav.hasPath) {
+			animation.CrossFade("Walk");
+		}else{
+			animation.CrossFade("Idle");
+		}
+		//}
+		//else
+		//{
+		//    nav.enabled = false;
+		//}
+	}
+}
